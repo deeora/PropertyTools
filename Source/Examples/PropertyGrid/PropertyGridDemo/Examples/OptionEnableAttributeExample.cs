228850e03061dfe8cffdcda0f1c1877563b46330
Deepak Arora <deepak.arora@dnv.com>
2024-07-12 11:30:21 +0200

    Implement RadioButton enable/disable feature using OptionEnableAttribute

---
+++ Source/Examples/PropertyGrid/PropertyGridDemo/Examples/OptionEnableAttributeExample.cs
@@ -0,0 +1,22 @@
+﻿namespace ExampleLibrary
+{
+    using System.ComponentModel;
+
+    [PropertyGridExample]
+    public class OptionEnableAttributeExample : Example
+    {
+        public enum TestEnumeration
+        {
+            Red,
+            [PropertyTools.DataAnnotations.OptionEnable("EnableGreenColorOption")]
+            Green,
+            Blue
+        }
+
+        [Category("Option enable attribute")]
+        [Description("Green color can be enabled or disabled by a 'Enable green color option'")]
+        public TestEnumeration Color { get; set; }
+
+        public bool EnableGreenColorOption{ get; set; }
+    }
+}
---
+++ Source/PropertyTools/DataAnnotations/OptionEnableAttribute.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="OptionEnableAttribute.cs" company="PropertyTools">
+//   Copyright (c) 2024 PropertyTools contributors
+// </copyright>
+// <summary>
+//   Specifies the property name used to enable or disable an option, represented as a radio button, in an enumeration.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PropertyTools.DataAnnotations
+{
+    using System;
+
+    /// <summary>
+    /// Specifies the property name used to enable or disable an option, represented as a radio button, in an enumeration.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    public class OptionEnableAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the name of the property that determines whether the option is enabled or disabled.
+        /// </summary>
+        public string PropertyName { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OptionEnableAttribute"/> class with the specified property name.
+        /// </summary>
+        /// <param name="propertyName">The name of the property that determines the enabled state of the option.</param>
+        public OptionEnableAttribute(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+    }
+}
